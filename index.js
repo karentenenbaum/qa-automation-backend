fc00bb99dded5ba81b4840fca72e08fc997bdf78
karentenenbaum <kjtenenbaum@gmail.com>
2021-04-16 16:45:05 -0300

    modified endpoints

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ const app = express()
 const port = 4000
 
 //register
-app.post('/user', (req, res) => {
-  console.log(req);
-  res.json({result: 'success'});
+app.post('/register', (req, res) => {
+    console.log('holaaaaa');
+    console.log(req);
+    res.json({result: 'success'});
 })
 
 //login  
-app.put('/user', (req, res) => {
-
-    var user = req.params
+app.put('/login', (req, res) => {
     res.json({result: 'success'})
-  })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
